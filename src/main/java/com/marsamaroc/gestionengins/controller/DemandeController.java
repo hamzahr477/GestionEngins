9c2e54364c3e5dffa30b1e88339fc3ee17333e9f
NouamaneDahhak <61422720+NouamaneDahhak@users.noreply.github.com>
2022-03-18 11:32:02 +0100

    change

--- src/main/java/com/marsamaroc/gestionengins/controller/DemandeController.java
+++ src/main/java/com/marsamaroc/gestionengins/controller/DemandeController.java
@@ -211,9 +211,9 @@ public class DemandeController {
     }
 
 
-    @RequestMapping(value="/engin",method= RequestMethod.GET)
-    DemandeCompletDTO ElisteEnginsEntree(){
-        Engin engin = enginService.getById("N102548");
+    @GetMapping(value="/engin/{idEngin}")
+    DemandeCompletDTO ElisteEnginsEntree(@PathVariable("idEngin") String idEngin){
+        Engin engin = enginService.getById(idEngin);
         return new DemandeCompletDTO(engin.getDerniereAffectation().getDemande(), Arrays.asList(new EnginDTO(engin,engin.getDerniereAffectation())));
     }
 
